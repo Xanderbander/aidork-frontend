c3bc4c8a94043cda202f5db53deadd1d363bde79
Xanderbander <klklthom580@gmail.com>
2024-11-05 21:20:40 -0600

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* * @type {import('tailwindcss').Config */
+module.exports = {
+  content: [
+    "./pages/**/*.js,tsx,jsx,xtsx",
+    "./components/*/*.js,tsx,jsx,x-tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
